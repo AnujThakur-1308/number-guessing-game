01d39d447cfe88588dfe0304d7d36746489ad934
AnujThakur-1308 <anuj.thakur.fea@gmail.com>
2023-07-25 13:24:48 +0530

    first commit

---
+++ number game.py
@@ -0,0 +1,14 @@
+###project 1
+import random
+guess=random.randint(1,100)
+for i in range(0,6):
+    num=int(input("enter your number"))
+    if num<guess:
+        print("it is low ")
+    elif  num>guess:
+        print("it is to high")
+    elif num==guess:
+        print("wow congo")
+        break
+else:
+    print("game over!\nTry again Later  - correct no is",guess)
